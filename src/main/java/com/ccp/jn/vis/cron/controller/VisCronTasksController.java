6f90c65b1543dfdc15115f8562ea83704c89903e
ccpjobsnow <69213361+ccpjobsnow@users.noreply.github.com>
2024-04-26 22:50:00 -0300

    .

--- src/main/java/com/ccp/jn/vis/cron/controller/VisCronTasksController.java
+++ src/main/java/com/ccp/jn/vis/cron/controller/VisCronTasksController.java
@@ -10,9 +10,9 @@ import com.ccp.implementations.file.bucket.gcp.CcpGcpFileBucket;
 import com.ccp.implementations.http.apache.mime.CcpApacheMimeHttp;
 import com.ccp.implementations.instant.messenger.telegram.CcpTelegramInstantMessenger;
 import com.ccp.implementations.json.gson.CcpGsonJsonHandler;
-import com.ccp.jn.async.business.JnAsyncBusinessNotifyError;
+import com.ccp.jn.async.business.support.JnAsyncBusinessNotifyError;
 import com.ccp.jn.cron.controller.CcpCronTasksController;
-import com.ccp.jn.vis.async.business.factory.CcpVisAsyncBusinessFactory;
+import com.ccp.vis.async.business.factory.CcpVisAsyncBusinessFactory;
 
 public class VisCronTasksController {
 	public static void main(String[] args) throws Exception {
@@ -30,9 +30,8 @@ public class VisCronTasksController {
 				new CcpGcpFileBucket()
 		);
 		
-		JnAsyncBusinessNotifyError jnAsyncBusinessNotifyError = new JnAsyncBusinessNotifyError();
 		String parameters = args[1];
 		String taskName = args[0];
-		CcpCronTasksController.main(jnAsyncBusinessNotifyError, taskName, parameters);
+		CcpCronTasksController.main(JnAsyncBusinessNotifyError.INSTANCE, taskName, parameters);
 	}
 }
